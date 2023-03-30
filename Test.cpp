77033e22de9c639b2bd91c16c1e8d77de9ddfa30
Tomer Gozlan <98852525+tomergozlan@users.noreply.github.com>
2023-03-30 19:41:22 +0300

    Add files via upload

--- Test.cpp
+++ Test.cpp
@@ -13,18 +13,18 @@ using namespace ariel;
 using namespace std;
 
 TEST_CASE("Test case 1: Initialize player with empty name") {
-    CHECK_THROW(Player p(""),std::invalid_argument);
+    CHECK_THROWS(Player (""));
 }
 
 TEST_CASE("Test case 2: Initialize game with same player") {
     Player p1("Alice");
-    CHECK_THROW(Game(p1,p1),std::invalid_argument);
+    CHECK_THROWS(Game(p1,p1));
 }
 
 TEST_CASE("Test case 3: Initialize game with same player names") {
     Player p1("Bob");
     Player p2("Bob");
-    CHECK_THROW(Game(p1,p2),std::invalid_argument);
+    CHECK_THROWS(Game(p1,p2));
 }
 
 TEST_CASE("Test case 4: Initialize game with different player names") {
@@ -58,37 +58,26 @@ TEST_CASE("Test case 7: Test player cannot playTurn() after playAll() ") {
     Player p2("Alice");
     Game game(p1,p2);
     game.playAll();
-    game.playTurn();
-    CHECK_THROW(game.playTurn(), std::logic_error);
+    CHECK_THROWS(game.playTurn());
 }
 
 TEST_CASE("Test case 8: Test printLastTurn() method cannot be used before playTurn() or playAll() methods") {
     Player p1("Bob");
     Player p2("Alice");
     Game game(p1, p2);
-    CHECK_THROW(game.printLastTurn(), std::logic_error);
+    CHECK_THROWS(game.printLastTurn());
     game.playTurn();
     game.printLastTurn();
     game.playAll();
     game.printLastTurn();
 }
 
-TEST_CASE("Test case 9: Test game ends correctly when one player has no cards left") {
+TEST_CASE("Test case 9: Test game ends correctly") {
     Player p1("Bob");
     Player p2("Alice");
     Game game(p1, p2);
-    bool is_game_over = false;
-    while (!is_game_over) {
-        try {
-            game.playTurn();
-        } catch (std::exception& e) {
-            is_game_over = true;
-        }
-        if (p1.stacksize()==0 || p2.stacksize()==0) {
-            is_game_over = true;
-        }
-    }
-    CHECK(game.printWiner() == "Bob" || game.printWiner() == "Alice");
+    game.playAll();
+    CHECK_NOTHROW(game.printWiner());
 }
 
 TEST_CASE("Test case 10: Test every Turn the players have 52 cards") {
@@ -101,19 +90,22 @@ TEST_CASE("Test case 10: Test every Turn the players have 52 cards") {
     }
 }
 
-    TEST_CASE("Test case 11:Checking that all the print functions work"){
-        Player p1("Alice");
-        Player p2("Bob");
-        Game game(p1,p2);
-        CHECK_NOTHROW(game.printLastTurn());// prints the name of the winning player
-        CHECK_NOTHROW(game.printStats());// for each player prints basic statistics: win rate, cards won, <other stats you want to print>. Also print the draw rate and amount of draws that happand. (draw within a draw counts as 2 draws. )
-        CHECK_NOTHROW(game.printLog());// prints all the turns played one line per turn (same format as game.printLastTurn())
-        CHECK_NOTHROW(game.printWiner());//prints last turn
-    }
+TEST_CASE("Test case 11:Checking that all the print functions work"){
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1,p2);
+    CHECK_NOTHROW(game.printStats());
+    CHECK_NOTHROW(game.printLog());
+    CHECK_NOTHROW(game.printWiner());
+}
 
-
-
-
-
-
-
+TEST_CASE("Test case 12:Checking after one turn"){
+    Player p1("Alice");
+    Player p2("Bob");
+    Game game(p1,p2);
+    game.playTurn();
+    CHECK(p1.stacksize()<26);
+    CHECK(p2.stacksize()<26);
+    CHECK(p1.stacksize()==p2.stacksize());
+    CHECK(p1.stacksize()+p2.stacksize()+p1.cardesTaken()+p2.cardesTaken()==52);
+}
